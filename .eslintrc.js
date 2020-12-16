eae3563856081b3b3cd2dc789864479cddfeef69
Adam <boudjemaa.adam@gmail.com>
2020-12-16 18:01:59 +0100

    add new npm scripts for lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,13 @@ module.exports = {
     commonjs: true,
     es6: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ["airbnb-base"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
     ecmaVersion: 2018,
   },
-  rules: {
-  },
+  rules: {},
 };
