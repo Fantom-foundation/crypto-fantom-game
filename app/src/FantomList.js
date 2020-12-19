dc0eb0741d8191a2ecc4d713f304e70eb85e0b02
Adam <boudjemaa.adam@gmail.com>
2020-12-19 18:35:12 +0100

    Update migration to mint NFT

---
+++ app/src/FantomList.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default ({ fantoms, uriBase }) => (
+  <table>
+    <thead>
+      <th>Id</th>
+      <th>Generation</th>
+      <th>Gene A</th>
+      <th>Gene B</th>
+      <th>Pic</th>
+    </thead>
+    <tbody>
+      {fantoms.map(fantom => (
+        <tr key={fantom.id}>
+          <td>{fantom.id}</td>
+          <td>{fantom.generation}</td>
+          <td>{fantom.geneA}</td>
+          <td>{fantom.geneB}</td>
+          <td>
+            <img
+              style={{width: "50px", height: "50px"}}
+              src={`${uriBase}/${fantom.id}`}
+            />
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
